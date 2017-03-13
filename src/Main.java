87b2694bc0625c77f09d312b6630954b7d740d0f
liuxiao <ipfoox@gmail.com>
2017-03-13 17:07:04 +0800

    fix:a line is now taken as three words

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,25 @@ import org.apache.spark.ml.clustering.KMeans;
 import org.apache.spark.ml.clustering.KMeansModel;
 import org.apache.spark.ml.feature.Word2Vec;
 import org.apache.spark.ml.feature.Word2VecModel;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import java.util.Properties;
+import org.apache.spark.sql.functions.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         SparkSession spark = SparkSession
                 .builder()
                 .config("SPARK_LOCAL_IP", "127.0.0.1")
-                .config("executor-memory", "2g")
                 .master("local[*]")
                 .getOrCreate();
         
         //获取数据，得到一个dataset ( features,model)
-        spark.read().csv("file:///Users/liuxiao/Downloads/test.csv").createOrReplaceTempView("test");
-        spark.sql("select `_c0` as word,collect_list(`_c0`) as features from test group by `_c0`").createOrReplaceTempView("vectors");
-        spark.read().csv("file:///Users/liuxiao/Downloads/test_1.csv").createOrReplaceTempView("test_1");
-        spark.sql("select `_c0` as model,`_c1` as word from test_1 ").createOrReplaceTempView("models");
-        Dataset<Row> ds = spark.sql("select a.features as features,b.model  as model from vectors a join models b on a.word = b.word");
 
+        spark.read().csv("file:///Users/liuxiao/word2vec/src/look1.csv").withColumnRenamed("_c0","line").createOrReplaceTempView("temp");
+        Dataset<Row> ds = spark.sql("select split(line,' ') as features,split(line,' ')[0] as model from temp");
 
         // word2vec
         Word2Vec word2Vec = new Word2Vec().setInputCol("features").setOutputCol("result").setWindowSize(10).setVectorSize(5).setMinCount(0);
@@ -33,6 +31,7 @@ public class Main {
         KMeans kmeans = new KMeans().setK(5).setSeed(1L).setFeaturesCol("result");
         KMeansModel kMeansModel = kmeans.fit(result);
         Dataset<Row> clusters = kMeansModel.transform(result).select("model", "prediction");
+        clusters.printSchema();
 
         //将clusters 这个 dataset写入数据库
         Properties connectionProperties = new Properties();
